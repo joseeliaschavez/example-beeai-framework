2e113ff4f526c58de756495108b789b943e041eb
Jose <rockmanjoe64@proton.me>
2025-06-04 21:12:36 -0700

    Added BeeAI typescript starter

---
+++ typescript-starter/src/agent.ts
@@ -0,0 +1,40 @@
+import "dotenv/config.js";
+import { ReActAgent } from "beeai-framework/agents/react/agent";
+import { FrameworkError } from "beeai-framework/errors";
+import { TokenMemory } from "beeai-framework/memory/tokenMemory";
+import { OpenMeteoTool } from "beeai-framework/tools/weather/openMeteo";
+import { DuckDuckGoSearchTool } from "beeai-framework/tools/search/duckDuckGoSearch";
+import { createConsoleReader } from "./helpers/reader.js";
+import { ChatModel } from "beeai-framework/backend/chat";
+
+const agent = new ReActAgent({
+  llm: await ChatModel.fromName(process.env.LLM_CHAT_MODEL_NAME as any),
+  memory: new TokenMemory(),
+  tools: [new OpenMeteoTool(), new DuckDuckGoSearchTool()],
+});
+
+const reader = createConsoleReader({ fallback: "What is the current weather in Las Vegas?" });
+for await (const { prompt } of reader) {
+  try {
+    const response = await agent
+      .run(
+        { prompt },
+        {
+          execution: {
+            maxIterations: 8,
+            maxRetriesPerStep: 3,
+            totalMaxRetries: 10,
+          },
+        },
+      )
+      .observe((emitter) => {
+        emitter.on("update", (data) => {
+          reader.write(`Agent 🤖 (${data.update.key}) :`, data.update.value);
+        });
+      });
+
+    reader.write(`Agent 🤖 :`, response.result.text);
+  } catch (error) {
+    reader.write(`Error`, FrameworkError.ensure(error).dump());
+  }
+}
